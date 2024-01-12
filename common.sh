df6140c015e32fcb3b2b248d50172e4fe962b66d
j4587698 <zhuce@jvxiang.com>
2024-01-12 18:49:44 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -5,7 +5,7 @@ FFMPEG_TARBALL=ffmpeg-$FFMPEG_VERSION.tar.bz2
 FFMPEG_TARBALL_URL=http://ffmpeg.org/releases/$FFMPEG_TARBALL
 
 FFMPEG_CONFIGURE_FLAGS=(
-    --disable-shared
+    --enable-shared
     --enable-static
     --enable-pic
 
